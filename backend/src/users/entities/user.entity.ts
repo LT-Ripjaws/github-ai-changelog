fd65aee2d47bf7c8e91e092e5d13cffa43841b07
LT-Ripjaws <nightangle676x@gmail.com>
2026-04-09 21:07:17 +0600

    refactor: some readability

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -6,14 +6,44 @@ import { RepoEntity } from '../../repos/entities/repo.entity';
 
 @Entity('users')
 export class UserEntity {
-  @PrimaryGeneratedColumn('uuid') id: string;
-  @Column({ name: 'github_id', unique: true }) githubId: string;
-  @Column() username: string;
-  @Column({ name: 'display_name', nullable: true }) displayName: string;
-  @Column({ name: 'avatar_url', nullable: true }) avatarUrl: string;
-  @Column({ nullable: true }) email: string;
-  @Column({ name: 'access_token' }) accessToken: string;
-  @CreateDateColumn({ name: 'created_at' }) createdAt: Date;
-  @UpdateDateColumn({ name: 'updated_at' }) updatedAt: Date;
-  @OneToMany(() => RepoEntity, (r) => r.user) repos: RepoEntity[];
+  @PrimaryGeneratedColumn('uuid') 
+  id: string;
+
+  @Column({ 
+    name: 'github_id', 
+    unique: true }) 
+  githubId: string;
+
+  @Column() 
+  username: string;
+
+  @Column({ 
+    name: 'display_name', 
+    nullable: true }) 
+  displayName: string;
+  
+  @Column({ 
+    name: 'avatar_url', 
+    nullable: true }) 
+  avatarUrl: string;
+
+  @Column({ 
+    nullable: true }) 
+  email: string;
+
+  @Column({ 
+    name: 'access_token' }) 
+  accessToken: string;
+
+  @CreateDateColumn({ 
+    name: 'created_at' }) 
+  createdAt: Date;
+
+  @UpdateDateColumn({ 
+    name: 'updated_at' }) 
+  updatedAt: Date;
+
+  @OneToMany(() => RepoEntity, (r) => r.user) 
+  repos: RepoEntity[];
+
 }
